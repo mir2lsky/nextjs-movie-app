647ead8a3b314651201abd3bd292909772b0c9ef
mir2lsky <mir2lsky@gmail.com>
2024-11-18 08:55:11 +0900

    comment add

--- components/movie.tsx
+++ components/movie.tsx
@@ -20,7 +20,8 @@ export default function Movie({ id, title, poster_path }: IMovieProps) {
   return (
     <div className={styles.movie}>
       <img src={poster_path} alt={title} onClick={onClick} />
-      {/* prefetch를 해주면 movies를 미리 패치해 주어서 속도가 향상됨 */}
+      {/* prefetch를 해주면 movies를 미리 패치해 주어서 속도가 향상되지만
+          잘못하면 부하가 커서 서버가 다운될 수 있으니 특정 페이지에 주의깊게 사용해야 함. */}
       <Link prefetch href={`/movies/${id}`}>
         {title}
       </Link>
